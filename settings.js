51f14f2f32a0807d6742527b841295c8b97fa487
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-04-02 19:59:58 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,8 +18,8 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Ifee By Putbotz" //namabot kalian
-global.ownername= "Ifeebotz" //nama kalian
+global.botname = "Putbotz" //namabot kalian
+global.ownername= "Putbotz" //nama kalian
 global.myweb ="https://chat.whatsapp.com/BzPu8WdbPqh4r3Hovb45Kx" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UCA_wQ8H5liNX9slHvAMg-BA" //bebas asal jan hapus
 global.github = "https://github.com/Putbotz" //bebas
@@ -31,7 +31,7 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["60147366955","12183020594","60134430667"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© ifeebotz by put' //sticker wm ubah
+global.packname = '© putbotz' //sticker wm ubah
 global.author = 'Di Buat Oleh Putbotz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
